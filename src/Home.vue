8bfac506720afacefbf4fc7b17ca99d4df0600ba
ztavruz <mr.stav.rus@mail.ru>
2019-01-09 01:06:33 +0300

    :x

---
+++ src/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Home page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+ 
+</style>
