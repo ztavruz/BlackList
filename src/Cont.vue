792b799be39364cb5f0652cda260456093d8fc05
ztavruz <mr.stav.rus@mail.ru>
2019-01-11 20:13:59 +0300

    MasterUpdate

---
+++ src/Cont.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Cont page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      fff:"dddaaa",
+      hhh: "adadada",
+      mmm: "cccccc"
+    }
+  }
+}
+</script>
+
+<style scoped>
+ 
+</style>
